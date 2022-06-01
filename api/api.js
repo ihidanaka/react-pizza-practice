9d8003272aec47ebfab49c79695569e3edefbc7c
ihidanaka <maks.kiselevk@yandex.ru>
2022-06-01 04:16:41 +0200

    api modified

--- api/api.js
+++ api/api.js
@@ -4,38 +4,46 @@ const User = require('../models/User');
 const Item = require('../models/Item');
 
 
-router.get('/users', (req, res) => {
-    User.find({})
+router.get('/users', async (req, res) => {
+    
+    await User.find({})
         .then(user => res.send(user))
 });
-
-router.get('/items?category=:categoryId', (req, res) => {
-    console.log(req.params.categoryId)
-    Item.find({category: Number(req.params.categoryId)})
-        .then(item => res.send(item))
+//'/items&category=:categoryId&sort=:sort'
+router.get('/items&category=:categoryId', async (req, res) => {
+    
+        if(req.params.categoryId == 0) await Item.find({}).orFail(
+            () => new Error(`Category with id: ${req.params.categoryId} not found`) 
+          )
+    .then(item => res.send(item)).catch((err) => res.status(404).send({ error: err.message })); else 
+    await Item.find({category: Number(req.params.categoryId)}).orFail(
+        () => new Error(`Category with id: ${req.params.categoryId} not found`) 
+      )
+        .then(item => res.send(item)).catch((err) => res.status(404).send({ error: err.message }))
+    
 });
 
 
-router.post('/users', (req, res) => {
-    User.create(req.body)
-        .then((user) => res.send(user))
+router.post('/users', async (req, res) => {
+    await User.create(req.body)
+        .then((user) => res.send(user)).catch((err) => res.status(404).send({ error: err.message }))
 
 });
-router.put('/users/:id', (req, res) => {
-    User.findByIdAndUpdate({ _id: req.params.id }, req.body)
-        .then(() => {
-            User.findOne({ _id: req.params.id })
+router.put('/users/:id', async (req, res) => {
+    await User.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        .then(async () => {
+            await User.findOne({ _id: req.params.id })
                 .then(user => res.send(
                     [
                         user['token'] ? user['token'] : { token: 'Key not found' },
                         user['name'] ? user['name'] : { name: 'Key not found' }
                     ]
-                ))
+                )).catch((err) => res.status(404).send({ error: err.message }))
         }
         )
 });
-router.delete('/users/:id', (req, res) => {
-    User.deleteOne({ _id: req.params.id })
+router.delete('/users/:id',async (req, res) => {
+    await User.deleteOne({ _id: req.params.id })
         .then(user => res.send(user))
 });
 module.exports = router;
