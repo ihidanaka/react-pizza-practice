c426c57c25220c637b82715b66ca874cb3d2662f
ihidanaka <maks.kiselevk@yandex.ru>
2022-06-16 13:46:44 +0200

    add services,controllers,router,models,jwt tokens test

---
+++ router/index.js
@@ -0,0 +1,51 @@
+const Router = require ('express').Router;
+const User = require("../models/user-model");
+const userController = require('../controllers/user-controller')
+const Item = require("../models/item-model");
+const Settings = require("../models/settings-model");
+const router = new Router();
+
+router.post('/register', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
+
+router.get("/settings", async (req, res) => {
+    await Settings.findOne({}).then((category) => res.send(category));
+  }); 
+  router.get("/items&category=:categoryId&sort=:sortType", async (req, res) => {
+    if (req.params.categoryId == 0)
+      await Item.find({})
+        .sort(req.params.sortType)
+        .orFail(
+          () => new Error(`Category with id: ${req.params.categoryId} not found`)
+        )
+  
+        .then((item) => res.send(item))
+        .catch((err) =>
+          res.status(404).send({
+  
+            error: err.message,
+          })
+        );
+    else
+      await Item.find({
+        category: Number(req.params.categoryId),
+      })
+        .sort(req.params.sortType)
+        .orFail(
+          () => new Error(`Category with id: ${req.params.categoryId} not found`)
+        )
+  
+        .then((item) => res.send(item))
+        .catch((err) =>
+          res.status(404).send({
+            
+            error: err.message,
+          })
+        );
+  });
+  
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,43 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const router = require('./router/index')
+const PORT = process.env.PORT || 4000; 
 const app = express();
-
-
-
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api', router);
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+        })
+        .then(val => {
+            console.log("connected to db " + val)
+        }, reason => {
+            console.log("rejected")
+        })
+        .catch(reason => {
+            console.log(reason)
+        })
+        app.listen(PORT, () => {
+            console.log(`server listening on ${PORT}`); 
+        });
 
 
-app.listen(4000, () => {
-    console.log('server listening on')
-});
-mongoose.connect('mongodb://ihidanaka:ihidanaka@5.187.0.127:27017/pizza-db', { useNewUrlParser: true })
-    .then(val => { console.log("connected to db " + val) }, reason => { console.log("rejected") })
-    .catch(reason => { console.log(reason) })
-app.use('/api', require('./api/api'))
+
+
+
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
+
